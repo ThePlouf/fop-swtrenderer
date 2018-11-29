45157de4548f64a507ae753f65ae86642b0e31e7
Plouf <plouf@pdty.be>
2018-11-29 23:51:19 +0800

    update copyright

--- src/main/java/be/pdty/fop/GCWrapper.java
+++ src/main/java/be/pdty/fop/GCWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Philippe Detournay
+ * Copyright 2018 Philippe Detournay
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
